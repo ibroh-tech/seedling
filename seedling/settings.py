fed7af9d2c6ab0b351606bb62b730310d31ce453
ibroh-tech <ibrohimmurodjonov515@gmail.com>
2025-08-29 16:54:24 +0500

    Update settings.py add media files

--- seedling/settings.py
+++ seedling/settings.py
@@ -163,6 +163,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+
+# Media files (user uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Ensure the favicon is served correctly
 FAVICON_PATH = os.path.join(STATIC_URL, 'favicon.ico')
 
